25561084168f565086636ac8b4c96e934d7f8e48
berto <alberto.serrano@gmail.com>
2016-04-14 23:27:45 +0200

    AES encryption working

--- app/pages/login/login-viewmodel.js
+++ app/pages/login/login-viewmodel.js
@@ -3,6 +3,7 @@ var observable = require("data/observable");
 var appSettings = require("application-settings");
 var dialogs = require("ui/dialogs");
 var frameModule = require("ui/frame");
+var CryptoJS = require("crypto-js");
 var LoginViewModel = (function (_super) {
     __extends(LoginViewModel, _super);
     function LoginViewModel() {
@@ -17,9 +18,15 @@ var LoginViewModel = (function (_super) {
     };
     LoginViewModel.prototype.onLogOn = function () {
         if (appSettings.hasKey("PASSWORD")) {
-            //Compare
-            if (this.password == appSettings.getString("PASSWORD")) {
-                //TODO:Navigate to sitelist
+            //Password already saved
+            //Decrypt it and compare
+            console.log(this.password);
+            var encryptedpwd = appSettings.getString("PASSWORD");
+            console.log(encryptedpwd);
+            var plainsavedpwdhex = CryptoJS.AES.decrypt(encryptedpwd, this.password);
+            var plainsavedpwd = plainsavedpwdhex.toString(CryptoJS.enc.Utf8);
+            console.log(plainsavedpwd);
+            if (this.password == plainsavedpwd) {
                 var topmost = frameModule.topmost();
                 topmost.navigate("pages/sitelist/sitelist-page");
             }
@@ -29,7 +36,11 @@ var LoginViewModel = (function (_super) {
         }
         else {
             //First time.Assign this one as pwd
-            appSettings.setString("PASSWORD", this.password);
+            var ciphertexthex = CryptoJS.AES.encrypt(this.password, this.password);
+            console.log(ciphertexthex);
+            var ciphertext = ciphertexthex.toString();
+            console.log(ciphertext);
+            appSettings.setString("PASSWORD", ciphertext);
         }
     };
     return LoginViewModel;
--- app/pages/login/login-viewmodel.ts
+++ app/pages/login/login-viewmodel.ts
@@ -1,7 +1,8 @@
 import observable = require("data/observable");
 import appSettings = require("application-settings");
 import dialogs = require("ui/dialogs");
-import frameModule = require("ui/frame");
+import frameModule = require("ui/frame")
+var CryptoJS = require("crypto-js");
 
 export class LoginViewModel extends observable.Observable {
     
@@ -21,9 +22,15 @@ export class LoginViewModel extends observable.Observable {
        
     public onLogOn(){
         if(appSettings.hasKey("PASSWORD")){
-            //Compare
-            if(this.password==appSettings.getString("PASSWORD")){
-                //TODO:Navigate to sitelist
+            //Password already saved
+            //Decrypt it and compare
+            //console.log(this.password);
+            var encryptedpwd = appSettings.getString("PASSWORD");
+            //console.log(encryptedpwd);
+            var plainsavedpwdhex = CryptoJS.AES.decrypt(encryptedpwd, this.password);
+            var plainsavedpwd =plainsavedpwdhex.toString(CryptoJS.enc.Utf8);  
+            //console.log(plainsavedpwd);
+            if(this.password==plainsavedpwd){
                 var topmost = frameModule.topmost();
                 topmost.navigate("pages/sitelist/sitelist-page");     
             }
@@ -32,7 +39,11 @@ export class LoginViewModel extends observable.Observable {
             }
         }else{
             //First time.Assign this one as pwd
-            appSettings.setString("PASSWORD",this.password);
+            var ciphertexthex = CryptoJS.AES.encrypt(this.password,this.password);
+            //console.log(ciphertexthex);
+            var ciphertext = ciphertexthex.toString();
+            //console.log(ciphertext);
+            appSettings.setString("PASSWORD",ciphertext);
         }
     }
 }
