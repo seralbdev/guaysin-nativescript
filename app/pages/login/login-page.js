88d66715b144eda09b2e63d377f4c96b5dc5d407
seralb <alberto.serrano@gmail.com>
2016-04-27 00:13:29 +0200

    Delete js result of compilation

--- app/pages/login/login-page.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var login_viewmodel_1 = require("./login-viewmodel");
-function navigatingTo(args) {
-    var page = args.object;
-    page.bindingContext = new login_viewmodel_1.LoginViewModel();
-}
-exports.navigatingTo = navigatingTo;
-//# sourceMappingURL=login-page.js.map
--- app/pages/sitelist/sitelist-page.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var sitelist_viewmodel_1 = require("./sitelist-viewmodel");
-function navigatingTo(args) {
-    var page = args.object;
-    page.bindingContext = new sitelist_viewmodel_1.SiteListViewModel();
-}
-exports.navigatingTo = navigatingTo;
-//# sourceMappingURL=sitelist-page.js.map
